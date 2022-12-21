866cf0c0c74252fdbb616dcf68f078b4287a1510
Arkady Schapov <sigma696@gmail.com>
2022-12-21 15:16:23 +0300

    create API schemes and models

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-console.log('movies');
+require('dotenv').config();
+const express = require('express');
+const https = require('https');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const fs = require('fs');
+const { log } = require('console');
+
+const userRoutes = require('./routes/users');
+const cardRoutes = require('./routes/cards');
+const signinRoute = require('./routes/signin');
+const signoutRoute = require('./routes/signout');
+const signupRoute = require('./routes/signup');
+const invalidRoutes = require('./routes/invalidURLs');
+const { handleAllErrors } = require('./errors/errors');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const {
+  PORT = 4000,
+  MONGODB_URI = 'mongodb://localhost:27017/moviesdb',
+  SSL_CRT_FILE,
+  SSL_KEY_FILE,
+} = process.env;
+
+const key = fs.readFileSync(SSL_KEY_FILE);
+const cert = fs.readFileSync(SSL_CRT_FILE);
+
+const app = express();
+const server = https.createServer({ key, cert }, app);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+mongoose.connect(MONGODB_URI, {
+  autoIndex: true,
+});
+
+const options = {
+  origin: [
+    'https://localhost:3000',
+  ],
+  credentials: true,
+};
+
+app.use('*', cors(options));
+app.use(limiter);
+app.use(requestLogger); // подключаем логгер запросов
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json()); // instead of body parser
+app.use('/signin', signinRoute);
+app.use('/signup', signupRoute);
+app.use(auth);
+app.use('/users', userRoutes);
+app.use('/cards', cardRoutes);
+app.use('/signout', signoutRoute);
+app.use('*', invalidRoutes);
+app.use(errorLogger); // подключаем логгер ошибок
+app.use(errors());
+app.use(handleAllErrors);
+
+server.listen(PORT, () => {
+  log(`App listening on HTTPS port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { NotAuthorizedError } = require('../errors/errors');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'DB: Email field is not valid',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      const error = new NotAuthorizedError('Неправильные почта или пароль');
+      if (!user) {
+        return Promise.reject(error);
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(error);
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
