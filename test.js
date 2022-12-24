f6c4f798a6ec9be7cdfb9d32b14476b3c9afaa9c
Arkady Schapov <sigma696@gmail.com>
2022-12-24 14:18:07 +0300

    remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question(`What's your name?`, name => {
-  console.log(`Hi ${name}!`);
-  readline.close();
-});
